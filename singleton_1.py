24cb1bdbef54dfa0d26f4b35d9362aa2a8faf2f4
Rameshtech17 <learnelectrotech@gmail.com>
2020-05-20 13:04:04 +0530

    Psycopg2 Learning

--- singleton_1.py
+++ singleton_1.py
@@ -28,19 +28,6 @@ class DBconnect():
         finally:
             return cur
 
-class DBclose():
-    __instance=None
-    def __new__(cls, database, user, password, host, port):
-        if cls.__instance is None:
-            cls.__instance = object.__new__(cls)
-        cls.__instance.database = database
-        cls.__instance.user = user
-        cls.__instance.password = password
-        cls.__instance.host = host
-        cls.__instance.port = port
-        return cls.__instance
-
-
 conn = DBconnect("postgres", "postgres", "test123", "127.0.0.1", "5432")
 conn1 = DBconnect("postgres", "postgres", "test123", "127.0.0.1", "5432")
 
